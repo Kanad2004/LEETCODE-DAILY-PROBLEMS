debc7986100c9caf214ffe710d966b2640316374
Kanad2004 <kanadkulkarni2013@gmail.com>
2025-01-17 17:40:02 +0530

    Bitwise XOR of All Pairings

---
+++ jan16.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int xorAllNums(vector<int> &nums1, vector<int> &nums2)
+    {
+        int ans = 0;
+        if (nums2.size() % 2 != 0)
+        {
+            for (int i = 0; i < nums1.size(); i++)
+            {
+                ans = ans ^ nums1[i];
+            }
+        }
+
+        if (nums1.size() % 2 != 0)
+        {
+            for (int i = 0; i < nums2.size(); i++)
+            {
+                ans = ans ^ nums2[i];
+            }
+        }
+
+        return ans;
+    }
+};
